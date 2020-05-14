24c1b1f353e78294ac18c9ce80a4613579d4832d
Eshwar-Chendra <61586489+Eshwar-Chendra@users.noreply.github.com>
2020-05-14 08:31:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,52 +13,32 @@ var num2=1;
 var num3=2;
 var num4=3;
 var num5=4;
-var correctImage,wrongImage;
-var score=0;
+
 
 function preload(){
   bookImage=loadImage("Sprites/book.png");
-  correctImage=loadImage("sprites/c.png");
-  wrongImage=loadImage("sprites/w.png");
 
 }
 function setup() {
   createCanvas(1500,1500);
   words=["outline","termination","missing","procrastinating","surprise"];
-  definitions=["A line or set of lines enclosing or indicating the shape of an object in a sketch.",
+  definitions=["A line or set of lines enclosing or indicating the shape of an object in a sketch or diagram.",
                "The action of terminating something or the fact of being terminated.",
                "Not present or included when expected or supposed to be.",
-               "Delay or postpone action;put off doing something.",
-               "an unexpected or astonishing event,fact."]
+               "Delay or postpone action; put off doing something.",
+               "an unexpected or astonishing event, fact."]
   box1=createSprite(870,700,40,40);
   box2=createSprite(970,700,40,40);
   box3=createSprite(1070,700,40,40);
   box4=createSprite(1170,700,40,40);
   box5=createSprite(1270,700,40,40);
-  correct=createSprite(750,550,20,20);
-  correct.addImage(correctImage);
-  wrong=createSprite(750,550,20,20);
-  wrong.addImage(wrongImage)
-  correct.scale=0.3;
-  wrong.scale=0.1;
-  correct.visible=false;
-  wrong.visible=false;
   
 }
 
 function draw() {
   background(bookImage); 
-  noStroke();
-  text("score: "+score,200,160);
   if(gameState==="start"){
-    correct.visible=false;
-    wrong.visible=false;
-    box1.shapeColor="#7F7F7F";
-    box2.shapeColor="#7F7F7F";
-    box3.shapeColor="#7F7F7F";
-    box4.shapeColor="#7F7F7F";
-    box5.shapeColor="#7F7F7F";
-    rand=Math.round(random(0,4));
+    rand=Math.round(random(0,3));
     
     randomNumber=[];
     while(randomNumber.length<5){
@@ -77,9 +57,8 @@ function draw() {
 
     gameState="play";
   }
-  
   textSize(30);
-  textFont("Comic Sans MS");
+  text
   fill(0);
   text(words[rand],370,350); 
   textSize(15);
@@ -93,81 +72,125 @@ function draw() {
   text(definitions[randomNumber[2]],770,400);
   text(definitions[randomNumber[3]],770,500);
   text(definitions[randomNumber[4]],770,600);
-  if (mousePressedOver(box1)){
-    if(num0===rand){
-      text("correct",970,900);
-      box1.shapeColor="green";
-      gameState="start";
-      correct.visible=true;
-      score=score+1;
-    }
-    else{
-      text("wrong",970,900);
-      box1.shapeColor="red";
-      gameState="start";
-      wrong.visible=true;
-    }
-  } 
-  if (mousePressedOver(box2)){
-    if(num1===rand){
-      text("correct",970,900);
-      box2.shapeColor="green";
-      gameState="start";
-      correct.visible=true;
-      score=score+1;
-    }
-    else{
-      text("wrong",970,900);
-      box2.shapeColor="red";
-      gameState="start";
-      wrong.visible=true;
-    }
+  if (mousePressedOver(box1)&&rand===0&&num0===rand){
+    text("correct",970,900);
+    box1.shapeColor="green";
   }
-  if (mousePressedOver(box3)){
-    if(num2===rand){
-      text("correct",970,900);
-      box3.shapeColor="green";
-      gameState="start";
-      correct.visible=true;
-      score=score+1;
-    }
-    else{
-      text("wrong",970,900);
-      box3.shapeColor="red";
-      gameState="start";
-      wrong.visible=true;
-    }
+  else{
+    text("wrong",970,900);
+    box2.shapeColor="red";
   }
-  if (mousePressedOver(box4)){
-    if(num3===rand){
-      text("correct",970,900);
-      box4.shapeColor="green";
-      gameState="start";
-      correct.visible=true;
-      score=score+1;
-    }
-    else{
-      text("wrong",970,900);
-      box4.shapeColor="red";
-      gameState="start";
-      wrong.visible=true;
-    }
+  if (mousePressedOver(box2)&&rand===1&&num1===rand){
+    text("correct",970,900);
+    box2.shapeColor="green";
   }
-  if (mousePressedOver(box5)){
-    if(num4===rand){
-      text("correct",970,900);
-      box5.shapeColor="green";
-      gameState="start";
-      correct.visible=true;
-      score=score+1;
-    }
-    else{
-      text("wrong",970,900);
-      box5.shapeColor="red";
-      gameState="start";
-      wrong.visible=true;
-    }
+  else{
+    text("wrong",970,900);
+    box2.shapeColor="red";
   }
+  if (mousePressedOver(box3)&&rand===2&&num2===rand){
+    text("correct",970,900);
+    box3.shapeColor="green";
+  }
+  else{
+    text("wrong",970,900);
+    box2.shapeColor="red";
+  }
+  if (mousePressedOver(box4)&&rand===3&&num3===rand){
+    text("correct",970,900);
+    box4.shapeColor="green";
+  }
+  else{
+    text("wrong",970,900);
+    box2.shapeColor="red";
+  }
+  if (mousePressedOver(box5)&&rand===4&&num4===rand){
+    text("correct",970,900);
+    box5.shapeColor="green";
+  }
+  else{
+    text("wrong",970,900);
+    box2.shapeColor="red";
+  }
+ /* if (mousePressedOver(box1)&&rand===0&&num0===rand){
+    text("wrong",970,900);
+    box1.shapeColor="red";
+  }
+  if (mousePressedOver(box1)&&rand===0&&num0===rand){
+    text("wrong",970,900);
+    box1.shapeColor="red";
+  }
+  if (mousePressedOver(box1)&&rand===0&&num0===rand){
+    text("wrong",970,900);
+    box1.shapeColor="red";
+  }
+  if (mousePressedOver(box1)&&rand===0&&num0===rand){
+    text("wrong",970,900);
+    box1.shapeColor="red";
+  }
+  if (mousePressedOver(box2)&&rand===1&&num1===rand){
+    text("wrong",970,900);
+    box2.shapeColor="red";
+  }
+  if (mousePressedOver(box2)&&rand===1&num1===rand){
+    text("wrong",970,900);
+    box2.shapeColor="red";
+  }
+  if (mousePressedOver(box2)&&rand===1&&num1===rand){
+    text("wrong",970,900);
+    box2.shapeColor="red";
+  }
+  if (mousePressedOver(box2)&&rand===1&&num1===rand){
+    text("wrong",970,900);
+    box2.shapeColor="red";
+  }
+  if (mousePressedOver(box3)&&rand===2&&num2===rand){
+    text("wrong",970,900);
+    box3.shapeColor="red";
+  }
+  if (mousePressedOver(box3)&&rand===2&&num2===rand){
+    text("wrong",970,900);
+    box3.shapeColor="red";
+  }
+  if (mousePressedOver(box3)&&rand===2&&num2===rand){
+    text("wrong",970,900);
+    box3.shapeColor="red";
+  }
+  if (mousePressedOver(box3)&&rand===2&&num2===rand){
+    text("wrong",970,900);
+    box3.shapeColor="red";
+  }
+  if (mousePressedOver(box4)&&rand===3&&num3===rand){
+    text("wrong",970,900);
+    box4.shapeColor="red";
+  }
+  if (mousePressedOver(box4)&&rand===3&&num3===rand){
+    text("wrong",970,900);
+    box4.shapeColor="red";
+  }
+  if (mousePressedOver(box4)&&rand===3&&num3===rand){
+    text("wrong",970,900);
+    box4.shapeColor="red";
+  }
+  if (mousePressedOver(box4)&&rand===3&&num3===rand){
+    text("wrong",970,900);
+    box4.shapeColor="red";
+  }
+  if (mousePressedOver(box5)&&rand===4&&num4===rand){
+    text("wrong",970,900);
+    box5.shapeColor="red";
+  }
+  if (mousePressedOver(box5)&&rand===4&&num4===rand){
+    text("wrong",970,900);
+    box5.shapeColor="red";
+  }
+  if (mousePressedOver(box5)&&rand===4&&num4===rand){
+    text("wrong",970,900);
+    box5.shapeColor="red";
+  }
+  if (mousePressedOver(box5)&&rand===4&&num4===rand){
+    text("wrong",970,900);
+  }*/
 
   drawSprites();
 }
